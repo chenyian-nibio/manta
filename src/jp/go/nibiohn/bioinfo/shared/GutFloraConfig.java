177d95d58cfcae42e3a735abb4c44ce83ab96e4d
Yi-An Chen <chenyian@nibiohn.go.jp>
2019-11-20 16:34:37 +0900

    update the documents

--- src/jp/go/nibiohn/bioinfo/shared/GutFloraConfig.java
+++ src/jp/go/nibiohn/bioinfo/shared/GutFloraConfig.java
@@ -4,8 +4,6 @@ public class GutFloraConfig {
 
 	public static final String PGSQL_PROP_FILE= "gutflora.db.properties";
 	
-	public static final String TABLE_HEADER_FILE= "table.header.properties";
-	
 	public static final String FOOTER = "Developed by the <a href=\"https://mizuguchilab.org/\" target=\"_new\" title=\"The Mizuguchi Laboratory\"><img src=\"https://targetmine.mizuguchilab.org/images/mizuguchilab.png\" border=\"0\" alt=\"Mizuguchi Lab\" style=\"vertical-align: middle;\"></a> @ <a href=\"http://www.nibiohn.go.jp/\" target=\"_new\" title=\"National Institutes of Biomedical Innovation, Health and Nutrition\"><img src=\"https://targetmine.mizuguchilab.org/images/nibiohn_logo_s.png\" border=\"0\" alt=\"NIBIOHN_logo\" style=\"vertical-align: middle;\"></a>";
 
 }
