2d14d7d80fcf5d7a14f3d612effb2201a85df854
KOMORI <komori@lifematics.co.jp>
2019-11-26 15:01:19 +0900

    Fix datasource loader

--- src/jp/go/nibiohn/bioinfo/server/DataSourceLoader.java
+++ src/jp/go/nibiohn/bioinfo/server/DataSourceLoader.java
@@ -17,39 +17,39 @@ public class DataSourceLoader{
     private static HikariConfig config;
 
     public static HikariDataSource getHikariDataSource() {
-		if (config == null) {
-            ///////////// Server version ///////////////
-            // backend = "postgresql";
-            // Properties props = new Properties();
-			// try {
-			// 	Class.forName("org.postgresql.Driver");
-			// 	props.load(GutFloraServiceImpl.class.getClassLoader().getResourceAsStream(GutFloraConfig.PGSQL_PROP_FILE));
-			// } catch (IOException e) {
-			// 	throw new RuntimeException("Problem loading properties '" + GutFloraConfig.PGSQL_PROP_FILE + "'", e);
-			// } catch (ClassNotFoundException e) {
-			// 	e.printStackTrace();
-			// }
-            // config = new HikariConfig(props);
-            ////////////////////////////////////////////
+		// if (config == null) {
+        //     /////////// Server version ///////////////
+        //     backend = "postgresql";
+        //     Properties props = new Properties();
+		// 	try {
+		// 		Class.forName("org.postgresql.Driver");
+		// 		props.load(GutFloraServiceImpl.class.getClassLoader().getResourceAsStream(GutFloraConfig.PGSQL_PROP_FILE));
+		// 	} catch (IOException e) {
+		// 		throw new RuntimeException("Problem loading properties '" + GutFloraConfig.PGSQL_PROP_FILE + "'", e);
+		// 	} catch (ClassNotFoundException e) {
+		// 		e.printStackTrace();
+		// 	}
+        //     config = new HikariConfig(props);
+        //     //////////////////////////////////////////
+        // }
 
-            ///////////// Desktop version ///////////////
-            backend = "sqlite";
-			config = new HikariConfig();
-			try {
-                Path classPath = Paths.get(DataSourceLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-                String dbPath = classPath.getParent().getParent().getParent().resolve("gutflora.db").toString();
-                if (dbPath == null) {
-                    dbPath = "gutflora.db";
-                }
-                System.out.println("Database path: " + dbPath);
-                config.setJdbcUrl("jdbc:sqlite:" + dbPath);
-			} catch (InvalidPathException e) {
-				e.printStackTrace();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
+        ///////////// Desktop version ///////////////
+        backend = "sqlite";
+        config = new HikariConfig();
+        try {
+            Path classPath = Paths.get(DataSourceLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            String dbPath = classPath.getParent().getParent().getParent().resolve("gutflora.db").toString();
+            if (dbPath == null) {
+                dbPath = "gutflora.db";
             }
-            ////////////////////////////////////////////
-		}
+            System.out.println("Database path: " + dbPath);
+            config.setJdbcUrl("jdbc:sqlite:" + dbPath);
+        } catch (InvalidPathException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        ////////////////////////////////////////////
         return new HikariDataSource(config);
     }
 }
